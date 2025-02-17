f3eed67b49d6aefe23d240f48cc42efd6b2c57a0
Luci-jjo <luccianocraft123@gmail.com>
2025-02-17 01:41:39 -0300

    ex4house

--- ex4.php
+++ ex4.php
@@ -1,7 +1,12 @@
 <?php
+    $nota = "B";
 
-    $nota = "B"
-
-    $aprovado = %nota
+    if ($nota === "I") {
+        echo "Reprovado";
+    } else if ($nota === "R" || $nota === "B" || $nota === "MB") {
+        echo "Aprovado";
+    } else {
+        echo "Nota inválida";
+    }
 
 ?>
